83a44642953b869884d6928d6321f7ab9343075d
nvtaikma <nvtai0202@gmail.com>
2023-10-04 16:43:49 +0700

    npm roudslide with firebase

---
+++ index.js
@@ -0,0 +1,36 @@
+function listen_change_value(idHtml, valueData) {
+  valueData.on("value", function (snapshot) {
+    const firebaseSliderValue = snapshot.val();
+    handleFirebaseSliderValueChange(idHtml, firebaseSliderValue);
+  });
+}
+
+function query_slider(id, value) {
+  $(document).ready(function () {
+    $(`#${id}`).roundSlider({
+      circleShape: "half-top",
+      sliderType: "min-range",
+      handleShape: "dot",
+      handleSize: "+5",
+      min: 0,
+      max: 180,
+      width: 25,
+      radius: 150,
+      value: 0,
+      change: function (e) {
+        const sliderValue = e.value;
+        value.set(sliderValue);
+      },
+    });
+  });
+}
+
+function handleFirebaseSliderValueChange(id, newValue) {
+  $(`#${id}`).roundSlider("setValue", newValue);
+}
+
+module.exports = {
+  listen_change_value,
+  query_slider,
+  handleFirebaseSliderValueChange,
+};
